59f2f1ef00858372d68fedfe6979d850aa12dbc2
B M Rauf <bmrauf@outlook.com>
2024-01-13 18:08:30 +0600

    Update Cloudy Day.py

--- Problem Solving (Basic)/Difficulty Level Medium/Cloudy Day.py
+++ Problem Solving (Basic)/Difficulty Level Medium/Cloudy Day.py
@@ -1,21 +1,6 @@
 # https://www.hackerrank.com/challenges/cloudy-day
 
-def maximumPeople(p, x, y, r):
-    if len(y) == 1 or len(y) == 0:
-        return sum(p) # if there is 1 cloud they can remove it and if it's 0 cloud nothing to remove.
-    else:
-        sunnyPeople = sum(p)
-        for l in range(len(x)): # len(x) or len(p) no of towns
-            count = 0
-            for c in range(len(y)): # len(y) no of clouds
-                if y[c] - r[c] <= x[l] <= y[c] + r[c]:
-                    if count == 0:
-                        sunnyPeople -= p[l]
-                        count+=1
-                    break             
-        return sunnyPeople
 
-print(maximumPeople([10,100], [5,100], [4], [1]))
 
 #four arrays representing the populations of each town(p),
 #locations of the towns(x), locations of the clouds(y),
